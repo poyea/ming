ec17997df057a4476fb0549d52796fcdf61d27e5
John Law <johnlaw.po@gmail.com>
2022-06-24 09:15:35 +0000

    chore: update license notice

--- include/ming/disjoint_set_union.hpp
+++ include/ming/disjoint_set_union.hpp
@@ -1,19 +1,19 @@
 //
-// ming/disjoint_set_union.hpp   Slightly advanced containers for C++
+// ming   Slightly advanced containers for C++
 // Copyright (C) 2022  John Law
 //
-// ming/disjoint_set_union.hpp is free software: you can redistribute it and/or modify
+// ming is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// ming/disjoint_set_union.hpp is distributed in the hope that it will be useful,
+// ming is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with ming/disjoint_set_union.hpp.  If not, see <https://www.gnu.org/licenses/>.
+// along with ming.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #pragma once
