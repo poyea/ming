83633ebb6b90ced4dacb99d2e740b9123b5822ed
John Law <johnlaw.po@gmail.com>
2025-07-19 22:15:20 +0000

    Add doc strings for trie

--- include/ming/trie.hpp
+++ include/ming/trie.hpp
@@ -26,8 +26,16 @@
 
 namespace ming {
 
+/**
+ * @brief A trie data structure for storing and retrieving strings
+ *
+ * @tparam Key The character type used for storing keys (defaults to char)
+ */
 template <typename Key = char>
 class Trie {
+  /**
+   * @brief Node class for the trie structure
+   */
   class TrieNode {
   public:
     TrieNode() : end_of_word(false){};
@@ -36,6 +44,12 @@ class Trie {
     std::unordered_map<Key, std::unique_ptr<TrieNode>> children;
   };
 
+  /**
+   * @brief Helper function to clone a trie structure
+   *
+   * @param from_node Source node to clone from
+   * @param to_node Destination node to clone to
+   */
   void clone(const std::unique_ptr<TrieNode> &from_node,
              std::unique_ptr<TrieNode> &to_node) {
     for (auto &[c, child_node] : from_node->children) {
@@ -52,8 +66,16 @@ class Trie {
   }
 
 public:
+  /**
+   * @brief Construct a new empty Trie
+   */
   Trie() noexcept : m_root(std::make_unique<TrieNode>()) {}
 
+  /**
+   * @brief Construct a Trie with initial words
+   *
+   * @param list List of words to insert into the trie
+   */
   Trie(std::initializer_list<std::string_view> list)
       : m_root(std::make_unique<TrieNode>()) {
     for (auto const &str : list) {
@@ -61,12 +83,31 @@ public:
     }
   }
 
+  /**
+   * @brief Destructor
+   */
   ~Trie() noexcept = default;
 
+  /**
+   * @brief Copy constructor
+   *
+   * @param other Trie to copy from
+   */
   Trie(const Trie &other) noexcept : Trie() { clone(other.m_root, m_root); }
 
+  /**
+   * @brief Move constructor
+   *
+   * @param other Trie to move from
+   */
   Trie(Trie &&other) noexcept : m_root(std::exchange(other.m_root, nullptr)) {}
 
+  /**
+   * @brief Copy assignment operator
+   *
+   * @param other Trie to copy from
+   * @return Trie& Reference to this object
+   */
   Trie &operator=(const Trie &other) noexcept {
     if (this == &other) {
       return *this;
@@ -74,11 +115,22 @@ public:
     return *this = Trie(other);
   }
 
+  /**
+   * @brief Move assignment operator
+   *
+   * @param other Trie to move from
+   * @return Trie& Reference to this object
+   */
   Trie &operator=(Trie &&other) noexcept {
     std::swap(m_root, other.m_root);
     return *this;
   }
 
+  /**
+   * @brief Insert a word into the trie
+   *
+   * @param word The word to insert
+   */
   void insert(std::string_view word) noexcept {
     auto *root = m_root.get();
     for (auto const &c : word) {
@@ -91,6 +143,13 @@ public:
     root->end_of_word = true;
   }
 
+  /**
+   * @brief Check if a word exists in the trie
+   *
+   * @param word The word to check for
+   * @return true If the word exists in the trie
+   * @return false If the word does not exist in the trie
+   */
   [[nodiscard]] bool is_word(std::string_view word) const noexcept {
     auto *root = m_root.get();
     for (auto const &c : word) {
@@ -103,6 +162,13 @@ public:
     return root->end_of_word;
   }
 
+  /**
+   * @brief Check if any word in the trie starts with the given prefix
+   *
+   * @param prefix The prefix to check for
+   * @return true If the prefix exists in the trie
+   * @return false If no word in the trie starts with the prefix
+   */
   [[nodiscard]] bool starts_with(std::string_view prefix) const noexcept {
     auto *root = m_root.get();
     for (auto const &c : prefix) {
@@ -116,6 +182,7 @@ public:
   }
 
 private:
+  /** @brief Root node of the trie */
   std::unique_ptr<TrieNode> m_root;
 };
 
